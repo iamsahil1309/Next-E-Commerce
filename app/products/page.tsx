aa17cfe8e4cf85e69d704b31ccce004e407b04f3
Sahil Singh <kswayam2000@gmail.com>
2025-03-20 13:11:23 +0530

    layout and zustand state management

---
+++ app/products/page.tsx
@@ -0,0 +1,19 @@
+import ProductList from "@/components/ProductList";
+import { stripe } from "@/lib/stripe";
+import React from "react";
+
+const page = async () => {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  return (
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+        All Products
+      </h1>
+      <ProductList products={products.data} />
+    </div>
+  );
+};
+
+export default page;
